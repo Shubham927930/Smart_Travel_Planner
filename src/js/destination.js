383c526faaebcfda31055af898e87130651b59f5
Shubham927930 <singhalshubham246@gmail.com>
2025-12-23 10:10:49 +0530

    Day 4 done

--- src/js/destination.js
+++ src/js/destination.js
@@ -3,6 +3,15 @@ const params = new URLSearchParams(window.location.search);
 const city = params.get("city");
 
 const cityName = document.getElementById("cityName");
+const loading = document.querySelector("#loading");
+const cityInfoBox = document.querySelector("#cityInfo");
+const errorBox = document.querySelector("#error");
+
+const lat = document.querySelector("#lat");
+const lon = document.querySelector("#lon");
+const country = document.querySelector("#country");
+const state = document.querySelector("#state");
+const population = document.querySelector("#population");
 
 let GEODB_API_KEY = "d579c9dfd1mshbeef850e3a64370p111538jsn83e2c70aaa5f";
 let GEODB_HOST = "wft-geo-db.p.rapidapi.com";
@@ -24,28 +33,40 @@ if (city) {
       );
       console.log(res);
 
-      if(!res.ok){
+      if (!res.ok) {
         console.log("API Response Not Fetched");
       }
 
       let data = await res.json();
       console.log(data);
 
-      if(!data.data || data.data.length === 0){
+      if (!data.data || data.data.length === 0) {
         console.log("City Not Found");
       }
 
       let cityData = data.data[0];
       console.log(cityData);
-      
 
-    //   Display City Info
-      
+      //   Display City Info
+
+      lat.textContent = cityData.latitude;
+      lon.textContent = cityData.longitude;
+      country.textContent = cityData.country;
+      state.textContent = cityData.region || "N/A";
+      population.textContent = cityData.population
+        ? cityData.population.toLocaleString()
+        : "N/A";
+
+      loading.classList.add("hidden");
+      cityInfoBox.classList.remove("hidden");
     } catch (error) {
       console.log(error);
+      loading.classList.add("hidden");
+      errorBox.classList.remove("hidden");
     }
   }
   fetchCityDetails(city);
 } else {
   cityName.textContent = "Unknown City";
+  loading.classList.add("hidden");
 }
